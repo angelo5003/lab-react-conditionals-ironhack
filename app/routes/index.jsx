d6063461879ed4fd28ecec897e935709d54c8a4d
Tony5300 <tony_tolen@ziggo.nl>
2023-05-30 15:00:35 +0200

    first commit

---
+++ app/routes/index.jsx
@@ -0,0 +1,40 @@
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>Welcome to Remix</h1>
+      <div>
+        <Button sayHi="say Hi">Click Here</Button>
+      </div>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/blog"
+            rel="noreferrer"
+          >
+            15m Quickstart Blog Tutorial
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/jokes"
+            rel="noreferrer"
+          >
+            Deep Dive Jokes App Tutorial
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+            Remix Docs
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+function Button(props) {
+  console.log("props:", props);
+  return <button>{props.children}</button>;
+}
